2366e5b255029993aaf6ae04526aebc950631862
”Michaela <michaelavietinghoff@Gmail.com>
2025-02-25 21:43:24 -0400

    Update: 1) Scheduler managment logic to make more sense for purposes 2) Add to Schedule class in order to be able to add class to the schedule

--- Schedule.java
+++ Schedule.java
@@ -3,13 +3,11 @@ import java.util.*;
 Track Classes for each respective day of the school week
  */
 public class Schedule {
-    public LinkedList monday;
-    public LinkedList tuesday;
-    public LinkedList wednesday;
-    public LinkedList thursday;
-    public LinkedList friday;
-
-
+    public LinkedList<Class> monday;
+    public LinkedList<Class> tuesday;
+    public LinkedList<Class> wednesday;
+    public LinkedList<Class> thursday;
+    public LinkedList<Class> friday;
 
     public Schedule(){
 
@@ -22,21 +20,22 @@ public class Schedule {
         for (int a = 0; a < slots; a++ ){
             System.out.println("What day do you want to add the slot too? (Monday, Tuesday, Wednesday, etc...)");
             String day = scn.nextLine();
+            System.out.println("");
             switch (day) {
                 case "Monday":
-
+                    monday = createClass(monday);
                     break;
                 case "Tuesday":
-
+                    tuesday = createClass(tuesday);
                     break;
                 case "Wednesday":
-
+                    wednesday = createClass(wednesday);
                     break;
                 case "Thursday":
-
+                    thursday = createClass(thursday);
                     break;
                 case "Friday":
-
+                    friday = createClass(friday);
                     break;
                 default:
                     System.out.println("Not a valid day. Try again...");
@@ -47,10 +46,27 @@ public class Schedule {
 
     }
 
+    //Retrieve class information and store info into the Class class and into each days respective Linked List
+    public LinkedList<Class> createClass(LinkedList<Class> classDays ){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("What is the class Name? Ex. CSCI-1234");
+        String classNumb = scn.nextLine();
+        System.out.println("What is the class Start Time on the 24 hours clock? Ex. 13:30");
+        String classStart = scn.nextLine();
+        System.out.println("What is the class End Time on the 24 hours clock? Ex. 19:30");
+        String classEnd = scn.nextLine();
+        System.out.println("What is the class Type? Type L for lecture, T for tutorial and L for Lab");
+        String classType = scn.nextLine();
+
+        Class newClass = new Class(classNumb, classStart, classEnd, classType);
+        classDays.add(newClass);
+        return classDays;
+    }
+
     //print the schedule for viewing
     public void printSched(){
-        System.out.println("|____|____|____|____|____|");
-    //print class numbers
-        System.out.println("|____|____|____|____|____|");
+        //System.out.printf("%-15s %-10s %-10s %-10s%n","Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
+
+
     }
 }
--- ScheduleController.java
+++ ScheduleController.java
@@ -14,19 +14,17 @@ public class ScheduleController {
         Scanner scn = new Scanner(System.in);
         System.out.println("Welcome to Dal Scheduler!");
         System.out.println("Type 1 to create a new schedule, 2 to print the schedule and 0 to end");
+        System.out.println("Type 3 to add classes to schedule");
         cmd = scn.nextLine();
 
         Schedule schedule = new Schedule();
+
         while (!cmd.equals("0")) {
-            switch (cmd) {
-                case "1":
-                    schedule = new Schedule();
-                    break;
-                case "2":
-                    schedule.printSched();
-                    break;
+           if (cmd.equals("1")){schedule = new Schedule();}
+            if (cmd.equals("2")){schedule.printSched();}
+            if (cmd.equals("3")){schedule.addSlots();}
             }
-        }
+
 
         System.out.println("Thank you for scheduling, have a good day!");
     }
